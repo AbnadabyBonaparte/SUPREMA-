b7e0c4fc0d6d2e7986c0eff2b7ad110f727a482f
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-12-08 19:40:29 -0300

    Create ConsultoriaPage.tsx

---
+++ src/app/routes/ConsultoriaPage.tsx
@@ -0,0 +1,13 @@
+// src/app/routes/ConsultoriaPage.tsx
+
+import SmartConsultation from '@/components/ai/SmartConsultation'
+import MatrixHub from '@/components/MatrixHub'
+
+export default function ConsultoriaPage() {
+  return (
+    <>
+      <MatrixHub />
+      <SmartConsultation />
+    </>
+  )
+}
